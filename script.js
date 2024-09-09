ddf166fdff02385f9ccff1e975554764324b9e93
Ronak Goyal <ronakgoyal1403@gmail.com>
2024-09-09 19:06:40 +0530

    fix image related problem

--- script.js
+++ script.js
@@ -31,19 +31,19 @@ async function checkWeather(city){
 
     switch(data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "/assets/cloud.png";
+            weather_img.src = "/Weather-App/assets/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "/assets/clear.png";
+            weather_img.src = "/Weather-App/assets/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "/assets/rain.png";
+            weather_img.src = "/Weather-App/assets/rain.png";
             break;
         case 'Mist':
-            weather_img.src = "/assets/mist.png";
+            weather_img.src = "/Weather-App/assets/mist.png";
             break;
         case 'Snow':
-            weather_img.src = "/assets/snow.png";
+            weather_img.src = "/Weather-App/assets/snow.png";
             break;
     }
 }
